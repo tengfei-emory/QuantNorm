fbb62a201a05cf08a9d826f3059757251dc9bb11
tengfei-emory <tfei@emory.edu>
2017-07-18 11:57:31 -0400

    zp stablization

--- R/zp.quantile.R
+++ R/zp.quantile.R
@@ -5,22 +5,22 @@ function(x,y)
 {
   o.x<-order(x)
   #r.x<-rank(x, ties.method = "random")
-  
+
   o.y<-order(y)
-  r.y<-rank(y, ties.method = "random")
-  
+  r.y<-rank(y, ties.method = "average")
+
   x<-x[o.x]
   y<-y[o.y]
-  
+
   x2<-x[x>0]
   y2<-y[y>0]
-  
+
   z.x<-seq(0,1,length.out=length(x2))
   z.y<-seq(0,1,length.out=length(y2))
-  
+
   new.y2<-approx(x=z.x, y=x2, xout=z.y)$y
   y[y>0]<-new.y2
   y<-y[r.y]
-  
+
   y
 }

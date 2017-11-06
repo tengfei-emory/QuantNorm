2a8e55253bcb5d102783a160b2fed94a30cdf10c
tengfei-emory <tfei@emory.edu>
2017-11-06 15:06:18 -0500

    rename data

--- R/ENCODE.r
+++ R/ENCODE.r
@@ -1,6 +1,6 @@
 #' Normalized ENCODE raw counts data for both human and mouse.
 #'
-#' @usage data(ENCODE.human.mouse)
+#' @usage data(ENCODE)
 #' @format A data frame with 10309 rows and 26 columns
 #' @source Reproduced according to Gilad, Yoav, and Orna Mizrahi-Man. "A reanalysis of mouse ENCODE comparative gene expression data." F1000Research 4 (2015).
-"ENCODE.human.mouse"
+"ENCODE"
--- R/data.r
+++ R/data.r
@@ -1,6 +1,6 @@
 #' Brain RNA-Seq data for both human and mouse.
 #'
-#' @usage data(humanmouse)
+#' @usage data(brain)
 #' @format A data frame with 15041 rows and 62 columns
 #' @source \url{http://web.stanford.edu/group/barres_lab/brainseq2/brainseq2.html}
-"humanmouse"
+"brain"
